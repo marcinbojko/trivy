8b02cf4beaefd9ab76f292c57380fdb1bbf3377c
Marcin Bojko <marcinbojko.pl@gmail.com>
2019-06-07 08:24:48 +0200

    Version 0.1.1
    2019-06-06

--- README.md
+++ README.md
@@ -1,3 +1,59 @@
-# trivy
+# Trivy
 
-Trivy package
+## Description
+
+A Simple and Comprehensive Vulnerability Scanner for Containers, Suitable for CI - [https://github.com/knqyf263/trivy](https://github.com/knqyf263/trivy)
+
+## Features
+
+* Install and uninstall via Chocolatey
+* Supports 32 and 64-bit version
+
+## Dependencies
+
+Due to proper usage and DB update, package `git` is required.
+
+## Changelog
+
+### 2019-05-27 Build 0.1.1
+
+* version 0.1.1
+* 64-bit version - [https://www.virustotal.com/#/file/1478078ae8957d697f576572f0f8f7e7abb6fe27fc62afdc16adcd1f511ab150/detection](https://www.virustotal.com/#/file/1478078ae8957d697f576572f0f8f7e7abb6fe27fc62afdc16adcd1f511ab150/detection)
+* 32-bit version - [https://www.virustotal.com/#/file/2585154eac2bdf6f273b580cfc7647a854ec6a8d98e2fb8696bace8d5d383545/detection](https://www.virustotal.com/#/file/2585154eac2bdf6f273b580cfc7647a854ec6a8d98e2fb8696bace8d5d383545/detection)
+
+## Usage
+
+### Package Parameters
+
+* /RefreshDB = (Yes/No) - If set to "Yes", after instalation Trivy will reset and update databases.
+
+  If not set "No" is default answer
+
+  Without `git` package instalation can take a while
+
+### Direct
+
+```cmd
+choco install trivy -y
+```
+
+or with DB update during instalation
+
+```cmd
+choco install trivy -y --params "/RefreshDB=Yes"
+```
+
+### YAML (Foreman, puppetlabs/chocolatey module)
+
+```yaml
+trivy:
+  ensure: latest
+  provider: chocolatey
+```
+
+or
+
+```yaml
+trivy:
+  ensure: latest
+```
---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,45 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName        = 'trivy'
+$version            = '0.1.1'
+$url64              = "https://github.com/knqyf263/trivy/releases/download/v"+$version+"/trivy_"+$version+"_Windows-64bit.zip"
+$url                = "https://github.com/knqyf263/trivy/releases/download/v"+$version+"/trivy_"+$version+"_Windows-32bit.zip"
+$checksum64         = "1478078ae8957d697f576572f0f8f7e7abb6fe27fc62afdc16adcd1f511ab150"
+$checksum           = "2585154eac2bdf6f273b580cfc7647a854ec6a8d98e2fb8696bace8d5d383545"
+$bindir             = Join-Path $env:ChocolateyInstall "lib\trivy\tools\trivy.exe"
+
+[regex]$refreshdbparams = “(?i)^(Yes|No)$”
+
+$pp=Get-PackageParameters
+if (!$pp['RefreshDB']) {$pp['RefreshDB']='No'}
+else {
+    if ($pp['RefreshDB'] -notmatch $refreshdbparams)
+        { Write-Output "Wrong value $($pp.RefreshDB) for parameter RefreshDB"
+         exit (1) }
+}
+$packageArgs = @{
+  packageName     = $packageName
+  fileType        = 'msi'
+  url64bit        = $url64
+  url             = $url
+  UnzipLocation   = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+  checksumType64  = 'sha256'
+  checksumType    = 'sha256'
+  checksum64      = $checksum64
+  checksum        = $checksum
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
+Write-Output $bindir
+try {
+    if ($($pp.RefreshDB) -eq "Yes") {
+      Write-Output "Updating Trivy databases - it can take a while"
+      & $bindir '--reset'
+      & $bindir '--refresh' '--quiet'
+    }
+  }
+catch {
+  # We don't care about updates, if they'll fail, do not fail package
+  exit (0)
+}
